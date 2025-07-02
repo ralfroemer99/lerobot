e143350b41d8cf4d9c0830a78c63a7fca6addf19
ralfroemer99 <ralf-roemer@gmx.de>
2025-07-02 09:58:56 +0200

    Synced with upstream

--- examples/test.py
+++ examples/test.py
@@ -1,28 +1,36 @@
 import numpy as np
 import torch
+from lerobot.common.policies.smolvla.modeling_smolvla import SmolVLAPolicy
 from lerobot.common.policies.diffusion.modeling_diffusion import DiffusionPolicy
 
 policy = DiffusionPolicy.from_pretrained("/home/ralf/Projects/lerobot/outputs/train/dp_test_ralf/checkpoints/last/pretrained_model")
+# policy = SmolVLAPolicy.from_pretrained("/home/ralf/Projects/lerobot/outputs/train/smolvla_test_ralf/checkpoints/last/pretrained_model")
 policy.reset()
-
 state=np.zeros(7)
 image=np.zeros((256, 256, 3))
 
 observation = {
     "observation.state": state,
     "observation.images.right_wrist_camera": image,
+    "task": "pick the lego block."
 }
 
 # Convert to pytorch format: channel first and float32 in [0,1] with batch dimension
 for name in observation:
-    observation[name] = torch.from_numpy(observation[name])
+    if isinstance(observation[name], np.ndarray):
+        observation[name] = torch.from_numpy(observation[name])
+    
     if "image" in name:
         observation[name] = observation[name].type(torch.float32) / 255
         observation[name] = observation[name].permute(2, 0, 1).contiguous()
-    else:
+    elif "state" in name:
         observation[name] = observation[name].type(torch.float32)
-    observation[name] = observation[name].unsqueeze(0)
-    observation[name] = observation[name].to('cuda')
+    
+    if not "task" in name:
+        observation[name] = observation[name].unsqueeze(0)
+        observation[name] = observation[name].to('cuda')
 
 action = policy.select_action(observation)
+
+print("Action:", action)
 
--- lerobot/scripts/train_custom.py
+++ lerobot/scripts/train_custom.py
@@ -13,6 +13,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+import os
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
 import logging
 import time
 from contextlib import nullcontext
